da9ab8c4d28dae684d2b56e4101388786ae8c2c6
Kochila <125907641+Kochila@users.noreply.github.com>
2023-10-23 18:55:40 -0700

    Minor Changes

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,66 +3,56 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import Student, Admin
+from App.models import Student, Admin, Competition, Participation
 from App.controllers import *
 
-
 LOGGER = logging.getLogger(__name__)
-
 '''
    Unit Tests
 '''
+
 class UnitTests(unittest.TestCase):
-   
-   'Student Unit Test
-   def test_new_student(self):
-        student = Student("bob", "bobpass")
-        assert student.username == "bob"
+  '''
+     Student Unit Test
+  '''
 
-   def test_set_points(self):
-        student = Student("bob", "bobpass")
-        student.set_points(15)
-        assert student.points == 15
-      
-   def test_set_ranking(self):
-        student = Student("bob", "bobpass")
-        student.set_points(10)
-        assert student.points == 10
-    
-   def test_previous_ranking(self):
-      student = Student("bob", "bobpass")
-      student.set_points(10)
-      student.set_previous_ranking(student.points)
-      student.set_points(5)
-      assert student.previous_ranking == 10
+  def test_new_student(self):
+    student = Student("Kim", "Possible")
+    assert student.username == "Kim"
+
+  def test_set_points(self):
+    student = Student("Kim", "Possible")
+    student.set_points(15)
+    assert student.points == 15
+
+  def test_set_ranking(self):
+    student = Student("Kim", "Possible")
+    student.set_points(10)
+    assert student.points == 10
+
+  def test_set_previous_ranking(self):
+    student = Student("Kim", "Possible")
+    student.set_points(10)
+    student.set_previous_ranking(student.points)
+    student.set_points(5)
+    assert student.previous_ranking == 10
+
+  '''
+  Admin Unit Test
+  '''
   
-   'Admin Unit Test
-   def test_new_admin(self):
-    admin = Admin("rob", "robpass", 1001)
-    assert admin.username == "rob" and admin.staff_id == 1001
+  def test_new_admin(self):
+    admin = Admin("Ron", "Stoppable", 1001)
+    assert admin.username == "Ron" and admin.staff_id == 1001
 
-   'Competition Unit Test
-   def test_new_competition(self):
-      competition = Competition("Comp", 1001)
-      assert competition.name == "Comp"
-
-
-    # pure function no side effects or integrations called
-   def test_get_json(self):
-       user = User("bob", "bobpass")
-       user_json = user.get_json()
-       self.assertDictEqual(user_json, {"id":None, "username":"bob"})
-    
-   def test_hashed_password(self):
-       password = "mypass"
-       hashed = generate_password_hash(password, method='sha256')
-       user = User("bob", password)
-       assert user.password != password
-
-   def test_check_password(self):
-       password = "mypass"
-       user = User("bob", password)
-       assert user.check_password(password)
+  '''
+  Competition Unit Test
+  '''
+  
+  def test_new_competition(self):
+    admin = Admin("Ron", "Stoppable", 1001)
+    competition = Competition("Comp", admin.staff_id)
+    assert competition.name == "Comp"
 
 '''
     Integration Tests
