7ee6873460ef0a6dc82302a6e2168146a0c15977
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-10-24 20:56:57 -0400

    Update Ranking.py
    Did not use Ranking class

--- App/models/Ranking.py
+++ App/models/Ranking.py
@@ -1,4 +1,4 @@
-from App.database import db
+"""from App.database import db
 from App.models import Participation
 
 class Ranking(db.Model):
@@ -12,4 +12,4 @@ class Ranking(db.Model):
     def update_total_points(self):
         user_participations = Participation.query.filter_by(user_id=self.user_id, competition_id=self.competition_id).all()
         total_points = sum(participation.points_earned for participation in user_participations)
-        self.total_points = total_points
+        self.total_points = total_points"""
